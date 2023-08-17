e3c1afcb0974e4e5cd793148e99f93257a2a1387
Luís Fernando Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2023-08-17 11:38:01 -0300

    feat: create method update user

--- src/routes/users.js
+++ src/routes/users.js
@@ -55,4 +55,42 @@ router.post('/', async (req, res) => {
   }
 });
 
-module.exports = router;
+router.put('/', async (req, res) => {
+  try {
+    const oldEmail = req.headers.email;
+    const { name, email } = req.body;
+
+    if(name.length < 3) {
+      return res.status(400).json({ error: 'Name should have more than 3 characters' });
+    }
+
+    if(email.length < 5 || !email.includes("@")) {
+      return res.status(400).json({ error: 'Email invalid' });
+    }
+
+    if(oldEmail.length < 5 || !oldEmail.includes("@")) {
+      return res.status(400).json({ error: 'Email invalid' });
+    }
+
+    const query = findByEmail(oldEmail);
+    const alreadyExists = await db.query(query);
+    if(!alreadyExists.rows[0]) {
+      return res.status(404).json({ error: 'User does not exits' })
+    }
+
+    const text = "UPDATE users SET name=$1, email=$2 WHERE email=$3 RETURNING *";
+    const values = [name, email, oldEmail];
+
+    const updateResponse = await db.query(text, values);
+    if(!updateResponse.rows[0]) {
+      return res.status(400).json({ error: "User not updated" });
+    }
+
+    return res.status(200).json(updateResponse.rows[0]);
+
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+})
+
+module.exports = router; 
